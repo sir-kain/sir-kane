1f3003217c1dcefb40aa443c2f8daa8df21f0b34
Sir Kane <ahmadouwalyndiaye@gmail.com>
2018-08-29 22:23:04 +0000

    reduire la taille des images de la section competence

--- src/components/competence/Competences.vue
+++ src/components/competence/Competences.vue
@@ -6,73 +6,73 @@
     <div class="line" v-if="langages && sectionStatus">
       <div class="box v-span">
         <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/html5.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/html5.png" alt="">
         <span>Html5</span>
         </div>
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/css.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/css.png" alt="">
         <span> CSS </span>
         </div>
       </div>
 
       <div class="box h-span">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/phplang.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/phplang.png" alt="">
         <span>Php</span>
         </div>
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/symfony.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/symfony.png" alt="">
         <span>Symfony</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/wordpress.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/wordpress.png" alt="">
         <span>Wordpress</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/laravel.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/laravel.png" alt="">
         <span>Laravel</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/java.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/java.png" alt="">
         <span>Java</span>
         </div>
       </div>
 
       <div class="box h-span v-span">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/js.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/js.png" alt="">
         <span>Javascript</span>
         </div>
         <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/vuejs.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/vuejs.png" alt="">
         <span>Vue.js</span>
         </div>
         <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/angular.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/angular.png" alt="">
           <span>Angular</span>
         </div>
         <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/angular.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/angular.png" alt="">
           <span>React</span>
         </div>
       </div>
 
       <div class="box h-span">
       <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/python.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/python.png" alt="">
         <span>Python</span>
         </div>
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/django.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/django.png" alt="">
         <span>Django</span>
         </div>
       </div>
@@ -80,43 +80,43 @@
     <div class="line" v-if="outils  && sectionStatus">
       <div class="box h-span">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/linux.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/linux.png" alt="">
         <span>Linux</span>
         </div>
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/intellij.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/intellij.png" alt="">
         <span>Intellij</span>
         </div>
       </div>
 
       <div class="box h-span">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/webpack.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/webpack.png" alt="">
         <span>Webpack</span>
         </div>
         <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/gulp.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/gulp.png" alt="">
         <span>Gulp</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/git.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/git.png" alt="">
         <span>Git</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/tools/docker.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/tools/docker.png" alt="">
         <span>Docker (virtualisation)</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="90px" width="100px" src="../../assets/competences/django.png" alt="">
+          <img height="80px" width="90px" src="../../assets/competences/django.png" alt="">
         <span>Django</span>
         </div>
       </div>
