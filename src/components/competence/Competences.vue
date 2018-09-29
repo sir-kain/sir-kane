9ccb251416f3fdc81f38732693487c4dd0cbd8d1
Ahmadou Waly NDIAYE <ahmadouwalyndiaye@gmail.com>
2018-09-29 21:42:49 +0000

    move images to cloudinary plateforme

--- src/components/competence/Competences.vue
+++ src/components/competence/Competences.vue
@@ -6,73 +6,73 @@
     <div class="line" v-if="langages && sectionStatus">
       <div class="box v-span">
         <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/html5.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/html5.png" alt="">
         <span>Html5</span>
         </div>
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/css.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/css.png" alt="">
         <span> CSS </span>
         </div>
       </div>
 
       <div class="box h-span">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/phplang.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/phplang.png" alt="">
         <span>Php</span>
         </div>
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/symfony.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/symfony.png" alt="">
         <span>Symfony</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/wordpress.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/wordpress.png" alt="">
         <span>Wordpress</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/laravel.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/laravel.png" alt="">
         <span>Laravel</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/java.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/java.png" alt="">
         <span>Java</span>
         </div>
       </div>
 
       <div class="box h-span v-span">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/js.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/js.png" alt="">
         <span>Javascript</span>
         </div>
         <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/vuejs.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/vuejs.png" alt="">
         <span>Vue.js</span>
         </div>
         <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/angular.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/angular.png" alt="">
           <span>Angular</span>
         </div>
         <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/angular.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/angular.png" alt="">
           <span>React</span>
         </div>
       </div>
 
       <div class="box h-span">
       <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/python.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/python.png" alt="">
         <span>Python</span>
         </div>
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/django.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/django.png" alt="">
         <span>Django</span>
         </div>
       </div>
@@ -80,43 +80,43 @@
     <div class="line" v-if="outils  && sectionStatus">
       <div class="box h-span">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/linux.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/linux.png" alt="">
         <span>Linux</span>
         </div>
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/intellij.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/intellij.png" alt="">
         <span>Intellij</span>
         </div>
       </div>
 
       <div class="box h-span">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/webpack.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/webpack.png" alt="">
         <span>Webpack</span>
         </div>
         <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/gulp.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/gulp.png" alt="">
         <span>Gulp</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/git.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/git.png" alt="">
         <span>Git</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/tools/docker.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/tools/docker.png" alt="">
         <span>Docker (virtualisation)</span>
         </div>
       </div>
 
       <div class="box">
          <div class="competence">
-          <img height="80px" width="90px" src="../../assets/competences/django.png" alt="">
+          <img height="80px" width="90px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/django.png" alt="">
         <span>Django</span>
         </div>
       </div>
@@ -124,21 +124,21 @@
     <div class="line-v2" v-if="contributions  && sectionStatus">
       <div class="box-contribution">
         <div class="contribution">
-          <img width="25px" src="../../assets/competences/symfony.png" alt="">
+          <img width="25px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/symfony.png" alt="">
           <a href="https://github.com/symfony/symfony/commits?author=sir-kain">symfony/symfony/commits?author=sir-kain</a>
         </div>
         <div class="contribution">
-          <img width="25px" src="../../assets/competences/symfony.png" alt="">
+          <img width="25px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/symfony.png" alt="">
           <a href="https://github.com/sonata-project/SonataAdminBundle/commits?author=sir-kain">SonataAdminBundle/commits?author=sir-kain</a>
         </div>
       </div>
       <div class="box-contribution">
         <div class="contribution">
-          <img width="25px" src="../../assets/competences/symfony.png" alt="">
+          <img width="25px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/symfony.png" alt="">
           <a href="https://github.com/symfony/symfony-docs/commits?author=sir-kain">symfony/symfony-docs/commits?author=sir-kain</a>
         </div>
         <div class="contribution">
-          <img width="25px" src="../../assets/competences/vuejs.png" alt="">
+          <img width="25px" src="https://res.cloudinary.com/dpdwdstqt/image/upload/v1538256294/portfolio/competences/vuejs.png" alt="">
           <a href="https://github.com/vuejs-fr/vuejs.org/commits?author=sir-kain">vuejs-fr/vuejs.org/commits?author=sir-kain</a>
         </div>
       </div>
