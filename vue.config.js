a8383f93925ff3a41bde3bb6180b20bc76d7d951
Ahmadou Waly NDIAYE <ahmadouwalyndiaye@gmail.com>
2018-11-25 02:09:02 +0000

    update @vue-cli-pma

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
-// vue.config.js
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
+        name: 'Sir Kane',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
         // configure the workbox plugin
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             // swSrc is required in InjectManifest mode.
-            swSrc: 'static/service-worker.js',
+            swSrc: 'dev/sw.js',
             // ...other Workbox options...
         }
     }
